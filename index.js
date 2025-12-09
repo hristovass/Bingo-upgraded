24c08e7300567392128279a5cc9c7b6d9668974f
rebeka2002 <rebeka.nikolova@student.um.si>
2025-12-09 16:34:24 +0100

    redesign leaderboard button

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+require('electron-reload')(__dirname, {
+  electron: require(`${__dirname}/node_modules/electron`)
+});
+
 require('dotenv').config();
 const {app, BrowserWindow, ipcMain} = require('electron/main');
 const path = require('node:path');
