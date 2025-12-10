7bcb703dec26d9ffa83789f87a704778ad59ebc2
Irina <irina.micovic@student.um.si>
2025-12-10 12:17:25 +0100

    Popravek 50/50

--- index.js
+++ index.js
@@ -204,7 +204,7 @@ ipcMain.handle('startGame', async (event, { group, categories, players }) => {
         // Fetch questions
         const { data, error } = await supabase
             .from('Questions')
-            .select('id, text, answers, image_path, category_id, age_group_id')
+            .select('id, text, answers, correct_answer, image_path, category_id, age_group_id')
             .eq('age_group_id', group)
             .in('category_id', categories);
 
@@ -216,7 +216,8 @@ ipcMain.handle('startGame', async (event, { group, categories, players }) => {
             options: q.answers,
             image_path: q.image_path,
             category_id: q.category_id,
-            age_group_id: q.age_group_id
+            age_group_id: q.age_group_id,
+            correct_answer: q.correct_answer
         }));
 
         currentGame = {
